e98151d79545d51a80922e822b6e88e6ca8a82c3
Mohammed Al Haj Rahmoun <mrahmoun93@gmail.com>
2017-03-21 13:59:43 +0200

    fix some bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.paths << Rails.root.join("app", "assets", "fonts")
+  config.i18n.default_locale = :ar
 end
