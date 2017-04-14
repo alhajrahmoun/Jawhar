9919ffd4096e4e03fe52891544ec9ec1e5a11379
Mohammed Al Haj Rahmoun <mrahmoun93@gmail.com>
2017-04-14 16:24:27 +0300

    fix ckeditor errors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w(ckeditor/*)
+Rails.application.config.assets.precompile += %w(ckeditor/)
 Rails.application.config.assets.precompile += %w(jquery.tagsinput.min.js)
 Rails.application.config.assets.precompile += %w(jquery.tagsinput.min.css)
 
