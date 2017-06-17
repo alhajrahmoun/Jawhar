e08481a0bd0a58312a7b6cb48d028f45121db0b4
Mohammed Al Haj Rahmoun <mrahmoun93@gmail.com>
2017-06-17 21:58:37 +0300

    remove adminPanel

--- app/controllers/admin/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::DashboardController <  Admin::BaseController
-	def index
-		@visit_count = Visit.count
-		@user_count = User.count
-		@active_users = User.active_users
-		@gist_count = Gist.count
-		@snippet_count = Snippet.count
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -28,10 +28,5 @@ Rails.application.routes.draw do
   resources :comments, only: [:create]
   resources :contacts, only: [:new, :create]
   root 'pages#index'
-
-  namespace :admin do 
-    root 'dashboard#index'
-    get 'dashboard/index'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
