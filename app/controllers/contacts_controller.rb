a643019a93f46e23d0d6c88d267b4c7b4aba37fd
Mohammed Al Haj Rahmoun <mrahmoun93@gmail.com>
2017-06-19 22:37:29 +0300

    show success alert after sending message with contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      respond_to do |format|
+        format.js
+      end
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
