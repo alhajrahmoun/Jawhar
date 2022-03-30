47cb7247e6b42acfe26ef829e13d71db619f2493
Mohamad <mrahmoun93@gmail.com>
2022-03-31 00:02:15 +0200

    upgrade to Rails 7

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 ruby '3.0.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6'
+gem 'rails', '~> 7'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.0'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,12 +8,17 @@ Bundler.require(*Rails.groups)
 
 module Jawhar
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
     require 'carrierwave/orm/activerecord'
 
-    config.load_defaults 6.1
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :sidekiq
   end
 end
