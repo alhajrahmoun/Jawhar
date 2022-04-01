118e15e179b391472daf97e81d0aa0255dde940d
Mohamad <mrahmoun93@gmail.com>
2022-04-01 02:04:00 +0200

    chore: js cleanup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ Rails.start();
 import "jquery-ui-dist/jquery-ui.min"
 import "overhang/dist/overhang.min"
 import '@client-side-validations/client-side-validations/src'
+import 'materialize-css/dist/js/materialize.min'
 
 import "trix"
 import "@rails/actiontext"
@@ -15,4 +16,18 @@ $(document).ready(function() {
     $("#owner a.add_fields").
     data('association-insertion-method', 'after').
     data('association-insertion-node', 'this');
+});
+
+$( document ).ready(function(){
+    $(".button-collapse").sidenav();
+    // Open Search Modal On Click
+    $(".search").click(function (){
+        $(".box-search").fadeIn(500);
+        $("body").css("overflow","hidden");
+    });
+    // Open Search Modal On Click
+    $(".close").click(function () {
+        $(".box-search").fadeOut(500);
+        $("body").css("overflow","scroll");
+    });
 });
